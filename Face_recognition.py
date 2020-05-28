bbad7cab09fe7dc13e8abfeedfce9d395d0bb38f
Sambhav Jain <52211108+Sambhavj821@users.noreply.github.com>
2020-05-28 13:07:08 +0530

    Update Face_recognition.py

--- Face_recognition.py
+++ Face_recognition.py
@@ -238,11 +238,11 @@ for i in range(0,10):
 cv2.destroyAllWindows()
 
 
-accuracy_file = open('/mlops2/accuracy.txt','w')
+accuracy_file = open('/mlops1/accuracy.txt','w')
 accuracy_file.write(str(scores[1]))
 accuracy_file.close()
 
-display_matter = open('/mlops2/display_matter.html','r+')
+display_matter = open('/mlops1/display_matter.html','r+')
 display_matter.read()
 display_matter.write('<pre>\n---------------------------------------------\n')
 display_matter.write(this_layer)
